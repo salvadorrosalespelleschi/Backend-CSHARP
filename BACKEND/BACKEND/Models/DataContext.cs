255467cc038e87321499ff40023822798c567bf9
Salvador Rosales Pelleschi <a2018114271@estudiantes.upsa.edu.bo>
2020-11-06 00:20:02 -0400

    a

---
+++ BACKEND/BACKEND/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BACKEND.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+               
+        }
+
+        public System.Data.Entity.DbSet<BACKEND.Models.Student> Students { get; set; }
+    }
+}
---
+++ BACKEND/BACKEND/Models/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BACKEND.Models
+{
+    public class Student
+    {
+        [Key]
+        [Required]
+        [Range(1, 9999)]
+        public int StudentID { get; set; }
+        [StringLength(30)]
+        [Required(ErrorMessage = "You must enter the field {0}")]
+        public string LastName { get; set; }
+        [StringLength(30, ErrorMessage = "The field {0} must be contained between {2} and {1} characters", MinimumLength = 2)]
+        [Required(ErrorMessage = "You must enter the field {0}")]
+        public string FirstName { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}",ApplyFormatInEditMode = false)]
+        public DateTime EnrollmentDate { get; set; }
+    }
+}
